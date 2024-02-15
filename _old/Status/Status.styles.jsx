50b1fbe239be4606116978185f1ebf1bd47a5524
tpsindyv <sindre.dyvik@turfpal.com>
2024-02-15 10:11:54 +0100

    Removed old design

--- _old/Status/Status.styles.jsx
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components"
-
-export const Content = styled.div`
-    height: 100%;
-`
--- _old/ui/Card/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Wrapper } from "./Card.styles"
-
-const Card = ({ children }) => {
-    return <Wrapper>{children}</Wrapper>
-}
-
-export default Card
