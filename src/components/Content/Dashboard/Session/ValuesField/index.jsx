a725c2ab79015b95ac1e8625c56905882c0d9e12
tpsindyv <sindre.dyvik@turfpal.com>
2024-03-22 11:59:05 +0100

    Bug fix in session

--- src/components/Content/Dashboard/Session/ValuesField/index.jsx
+++ src/components/Content/Dashboard/Session/ValuesField/index.jsx
@@ -1,64 +1,64 @@
-import React from "react"
+import React from 'react'
 
-import { Container } from "./ValuesField.styles"
+import { Container } from './ValuesField.styles'
 
 // Icons
-import CloudOutlinedIcon from "@mui/icons-material/CloudOutlined"
-import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined"
-import DeviceThermostatOutlinedIcon from "@mui/icons-material/DeviceThermostatOutlined"
-import WaterDropOutlinedIcon from "@mui/icons-material/WaterDropOutlined"
-import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined"
-import HeatIcon from "../../../../../assets/icons/heat"
-import Humidity from "../../../../../assets/icons/Humidity"
-import SoilTemperature from "../../../../../assets/icons/SoilTemperature"
+import CloudOutlinedIcon from '@mui/icons-material/CloudOutlined'
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined'
+import DeviceThermostatOutlinedIcon from '@mui/icons-material/DeviceThermostatOutlined'
+import WaterDropOutlinedIcon from '@mui/icons-material/WaterDropOutlined'
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined'
+import HeatIcon from '../../../../../assets/icons/heat'
+import Humidity from '../../../../../assets/icons/Humidity'
+import SoilTemperature from '../../../../../assets/icons/SoilTemperature'
 
-import Value from "./Value"
+import Value from './Value'
 
 function ValuesField({ allValues }) {
-    return (
-        <Container>
-            <Value
-                value={Math.round(allValues.values.light)}
-                unit={"µMol"}
-                Icon={WbSunnyOutlinedIcon}
-            />
-            <Value
-                value={Math.round(allValues.values.ambient_temperature)}
-                unit={"°C"}
-                Icon={DeviceThermostatOutlinedIcon}
-            />
-            <Value
-                value={Math.round(allValues.values.soil_moisture)}
-                unit={"%"}
-                Icon={Humidity}
-            />
-            <Value
-                value={Math.round(allValues.values.temperature)}
-                unit={"°C"}
-                Icon={SoilTemperature}
-            />
-            <Value
-                value={Math.round(allValues.values.led_zone1_dim)}
-                unit={"%"}
-                Icon={LightbulbOutlinedIcon}
-            />
-            <Value
-                value={allValues.statuses.heat_zone1}
-                unit={""}
-                Icon={HeatIcon}
-            />
-            <Value
-                value={allValues.statuses.irrigation_solenoid}
-                unit={""}
-                Icon={WaterDropOutlinedIcon}
-            />
-            <Value
-                value={allValues.values.co2}
-                unit={"ppm"}
-                Icon={CloudOutlinedIcon}
-            />
-        </Container>
-    )
+	return (
+		<Container>
+			<Value
+				value={Math.round(allValues.values.light)}
+				unit={'µMol'}
+				Icon={WbSunnyOutlinedIcon}
+			/>
+			<Value
+				value={Math.round(allValues.values.temperature)}
+				unit={'°C'}
+				Icon={DeviceThermostatOutlinedIcon}
+			/>
+			<Value
+				value={Math.round(allValues.values.soil_moisture)}
+				unit={'%'}
+				Icon={Humidity}
+			/>
+			<Value
+				value={Math.round(allValues.values.soil_temperature)}
+				unit={'°C'}
+				Icon={SoilTemperature}
+			/>
+			<Value
+				value={Math.round(allValues.values.led_zone1_dim)}
+				unit={'%'}
+				Icon={LightbulbOutlinedIcon}
+			/>
+			<Value
+				value={allValues.statuses.heat_zone1}
+				unit={''}
+				Icon={HeatIcon}
+			/>
+			<Value
+				value={allValues.statuses.irrigation_solenoid}
+				unit={''}
+				Icon={WaterDropOutlinedIcon}
+			/>
+			<Value
+				value={allValues.values.co2}
+				unit={'ppm'}
+				Icon={CloudOutlinedIcon}
+			/>
+		</Container>
+	)
 }
 
 export default ValuesField
--- src/components/Content/Dashboard/index.jsx
+++ src/components/Content/Dashboard/index.jsx
@@ -1,141 +1,141 @@
-import React, { useState } from "react"
+import React, { useState } from 'react'
 // Styles
-import { Wrapper, ButtonsArea, LinkItem } from "./Dashboard.styles"
+import { Wrapper, ButtonsArea, LinkItem } from './Dashboard.styles'
 
 //Components
-import Btn from "../../UI/Btn"
-import ControlTiles from "./ControlTiles"
+import Btn from '../../UI/Btn'
+import ControlTiles from './ControlTiles'
 
 // Images
-import AutorenewOutlinedIcon from "@mui/icons-material/AutorenewOutlined"
-import BackHandOutlinedIcon from "@mui/icons-material/BackHandOutlined"
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined"
+import AutorenewOutlinedIcon from '@mui/icons-material/AutorenewOutlined'
+import BackHandOutlinedIcon from '@mui/icons-material/BackHandOutlined'
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
 
 // API
-import Session from "./Session"
-import { useContext } from "react"
-import { AllValuesContext } from "../../../store/context/allValues-context"
+import Session from './Session'
+import { useContext } from 'react'
+import { AllValuesContext } from '../../../store/context/allValues-context'
 
 function Dashboard({}) {
-    const { data: allValues, onCommand } = useContext(AllValuesContext)
-    const [states, setStates] = useState({
-        newCommand: false,
-        setpoints: "default",
-        calendar: allValues.statuses.calendar,
-    })
+	const { data: allValues, onCommand } = useContext(AllValuesContext)
+	const [states, setStates] = useState({
+		newCommand: false,
+		setpoints: 'default',
+		calendar: allValues.statuses.calendar,
+	})
 
-    const handleAuto = () => {
-        setStates((prev) => {
-            return { ...prev, auto: true, manual: false, newCommand: true }
-        })
-    }
+	const handleAuto = () => {
+		setStates((prev) => {
+			return { ...prev, auto: true, manual: false, newCommand: true }
+		})
+	}
 
-    const handleManual = () => {
-        setStates((prev) => {
-            return { ...prev, auto: false, manual: true, newCommand: false }
-        })
-    }
+	const handleManual = () => {
+		setStates((prev) => {
+			return { ...prev, auto: false, manual: true, newCommand: false }
+		})
+	}
 
-    const handleStartStop = () => {
-        // if session is already active, set mode to 'Manual' to stop session.
-        if (allValues?.statuses?.session) {
-            onCommand(
-                {
-                    commands: {
-                        auto: false,
-                        manual: true,
-                        led_zone1_on: false,
-                        led_zone2_on: false,
-                        heat_zone1: false,
-                        heat_zone2: false,
-                        heat_zone3: false,
-                        dim: false,
-                        calendaroff: true,
-                    },
-                    setpoints: {
-                        led_zone1_dim_man: 0,
-                        led_zone2_dim_man: 0,
-                    },
-                },
-                100
-            )
-            return
-        }
+	const handleStartStop = () => {
+		// if session is already active, set mode to 'Manual' to stop session.
+		if (allValues?.statuses?.session) {
+			onCommand(
+				{
+					commands: {
+						auto: false,
+						manual: true,
+						led_zone1_on: false,
+						led_zone2_on: false,
+						heat_zone1: false,
+						heat_zone2: false,
+						heat_zone3: false,
+						dim: false,
+						calendaroff: true,
+					},
+					setpoints: {
+						led_zone1_dim_man: 0,
+						led_zone2_dim_man: 0,
+					},
+				},
+				100
+			)
+			return
+		}
 
-        let command = {
-            auto: states.auto,
-            manual: states.manual,
-            default: states.setpoints === "default",
-            user_defined1: states.setpoints === "user_defined1",
-            user_defined2: states.setpoints === "user_defined2",
-            user_defined3: states.setpoints === "user_defined3",
-            calendaron: states.calendar,
-            calendaroff: !states.calendar,
-        }
+		let command = {
+			auto: states.auto,
+			manual: states.manual,
+			default: states.setpoints === 'default',
+			user_defined1: states.setpoints === 'user_defined1',
+			user_defined2: states.setpoints === 'user_defined2',
+			user_defined3: states.setpoints === 'user_defined3',
+			calendaron: states.calendar,
+			calendaroff: !states.calendar,
+		}
 
-        onCommand(
-            {
-                commands: { ...command },
-            },
-            100
-        )
-    }
+		onCommand(
+			{
+				commands: { ...command },
+			},
+			100
+		)
+	}
 
-    const onToggleSchedule = (state) => {
-        setStates((prev) => {
-            return { ...prev, calendar: state }
-        })
-    }
+	const onToggleSchedule = (state) => {
+		setStates((prev) => {
+			return { ...prev, calendar: state }
+		})
+	}
 
-    const onSelectSetpoints = (setpoints) => {
-        setStates((prev) => {
-            return { ...prev, setpoints }
-        })
-    }
+	const onSelectSetpoints = (setpoints) => {
+		setStates((prev) => {
+			return { ...prev, setpoints }
+		})
+	}
 
-    let state = allValues.statuses.session
+	let state = allValues.statuses.mode === 'auto'
 
-    if (states.newCommand) {
-        state = states.auto
-    }
+	if (states.newCommand) {
+		state = states.auto
+	}
 
-    return (
-        <Wrapper>
-            <ButtonsArea>
-                <Btn svgSize={24} selected={state} onClick={handleAuto}>
-                    <AutorenewOutlinedIcon /> Auto
-                </Btn>
-                <Btn svgSize={24} selected={!state} onClick={handleManual}>
-                    <BackHandOutlinedIcon /> Manual
-                </Btn>
-            </ButtonsArea>
-            {state ? (
-                <>
-                    <Session
-                        onStartStop={handleStartStop}
-                        onToggleSchedule={onToggleSchedule}
-                        onSelectSetpoints={onSelectSetpoints}
-                        allValues={allValues}
-                        tempStates={states}
-                    />
-                    <LinkItem
-                        to={"settings"}
-                        state={{ headerText: "Auto Mode > Settings" }}
-                    >
-                        <Btn
-                            backgroundColorDeselected={"var(--lightGrey)"}
-                            textColorDeselected={"var(--turfpalColor)"}
-                            customFont={"var(--turfpalFontBold)"}
-                            onClick={() => {}}
-                        >
-                            <SettingsOutlinedIcon /> Settings
-                        </Btn>
-                    </LinkItem>
-                </>
-            ) : null}
-            <ControlTiles />
-        </Wrapper>
-    )
+	return (
+		<Wrapper>
+			<ButtonsArea>
+				<Btn svgSize={24} selected={state} onClick={handleAuto}>
+					<AutorenewOutlinedIcon /> Auto
+				</Btn>
+				<Btn svgSize={24} selected={!state} onClick={handleManual}>
+					<BackHandOutlinedIcon /> Manual
+				</Btn>
+			</ButtonsArea>
+			{allValues.statuses.session || states.auto ? (
+				<>
+					<Session
+						onStartStop={handleStartStop}
+						onToggleSchedule={onToggleSchedule}
+						onSelectSetpoints={onSelectSetpoints}
+						allValues={allValues}
+						tempStates={states}
+					/>
+					<LinkItem
+						to={'settings'}
+						state={{ headerText: 'Auto Mode > Settings' }}
+					>
+						<Btn
+							backgroundColorDeselected={'var(--lightGrey)'}
+							textColorDeselected={'var(--turfpalColor)'}
+							customFont={'var(--turfpalFontBold)'}
+							onClick={() => {}}
+						>
+							<SettingsOutlinedIcon /> Settings
+						</Btn>
+					</LinkItem>
+				</>
+			) : null}
+			<ControlTiles />
+		</Wrapper>
+	)
 }
 
 export default Dashboard
