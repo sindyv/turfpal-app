a73fd13cc6eb3c1f3e4a53ecc0c93155451277b9
sindyv <91290823+sindyv@users.noreply.github.com>
2024-08-12 16:51:24 +0200

    minor changes

--- src/components/Content/Schedule/Timer/Switches/index.jsx
+++ src/components/Content/Schedule/Timer/Switches/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import Card from '../../../../UI/Card'
-import AutorenewOutlinedIcon from '@mui/icons-material/AutorenewOutlined'
-import { Switch } from '@mui/material'
-import { CardHeader, CardContent, SwitchField } from './Switches.styles'
+import React from "react"
+import { useTranslation } from "react-i18next"
+import Card from "../../../../UI/Card"
+import AutorenewOutlinedIcon from "@mui/icons-material/AutorenewOutlined"
+import { Switch } from "@mui/material"
+import { CardHeader, CardContent, SwitchField } from "./Switches.styles"
 
 function Switches({ onSetState, states, allValues }) {
 	const { t } = useTranslation()
@@ -11,54 +11,50 @@ function Switches({ onSetState, states, allValues }) {
 		<Card>
 			<CardHeader>
 				<AutorenewOutlinedIcon />
-				{t('generic.action')}
+				{t("generic.action")}
 			</CardHeader>
 			<CardContent>
 				<SwitchField>
-					{t('light.lighting')}
+					{t("light.lighting")}
 					<Switch
 						disabled={allValues.statuses.timer_active}
 						checked={states.lighting}
-						color='whiteBackground'
-						size='small'
-						onChange={() =>
-							onSetState('lighting', !states.lighting)
-						}
+						color="whiteBackground"
+						size="small"
+						onChange={() => onSetState("lighting", !states.lighting)}
 					/>
 				</SwitchField>
 				<SwitchField>
-					{t('heat.heating')}
+					{t("heat.heating")}
 					<Switch
 						disabled={allValues.statuses.timer_active}
-						color='whiteBackground'
-						size='small'
+						color="whiteBackground"
+						size="small"
 						checked={states.heating}
-						onChange={() => onSetState('heating', !states.heating)}
+						onChange={() => onSetState("heating", !states.heating)}
 					/>
 				</SwitchField>
-				{allValues?.statuses?.irrigation_solenoid ?? false ? (
+				{allValues?.statuses.hasOwnProperty("zone1_irrigation") ? (
 					<SwitchField>
-						Irrigation{' '}
+						{t("irrigation.irrigation")}
 						<Switch
 							disabled={allValues.statuses.timer_active}
-							color='whiteBackground'
-							size='small'
+							color="whiteBackground"
+							size="small"
 							checked={states.irrigation}
-							onChange={() =>
-								onSetState('irrigation', !states.irrigation)
-							}
+							onChange={() => onSetState("irrigation", !states.irrigation)}
 						/>
 					</SwitchField>
 				) : null}
-				{allValues?.statuses?.co2_solenoid ?? false ? (
+				{allValues?.statuses.hasOwnProperty("zone1_co2") ? (
 					<SwitchField>
-						Co2{' '}
+						Co2{" "}
 						<Switch
 							disabled={allValues.statuses.timer_active}
-							color='whiteBackground'
-							size='small'
+							color="whiteBackground"
+							size="small"
 							checked={states.co2}
-							onChange={() => onSetState('co2', !states.co2)}
+							onChange={() => onSetState("co2", !states.co2)}
 						/>
 					</SwitchField>
 				) : null}
